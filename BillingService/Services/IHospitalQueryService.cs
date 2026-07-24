a361c53f2b34fa1890bc19870f0f368292ddec29
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-07-24 15:16:44 +0530

    Add initial implementation of BillingService with DbContext and service classes

---
+++ BillingService/Services/IHospitalQueryService.cs
@@ -0,0 +1,10 @@
+﻿using BillingService.Data;
+
+namespace BillingService.Services;
+
+public interface IHospitalQueryService
+{
+    HmsDbContext CreateHmsContext(string csName);
+    LisDbContext CreateLisContext(string csName);
+    ViphaDbContext CreateViphaContext(string csName);
+}
