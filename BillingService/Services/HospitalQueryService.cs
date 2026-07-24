bcfeff2fa456face65077b97b9f72317f2d4528e
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-07-24 15:16:44 +0530

    Add initial implementation of BillingService with DbContext and service classes

---
+++ BillingService/Services/HospitalQueryService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BillingService.Data;
+
+namespace BillingService.Services;
+
+public class HospitalQueryService : IHospitalQueryService
+{
+    private readonly IConfiguration _config;
+    public HospitalQueryService(IConfiguration config) => _config = config;
+
+    public HmsDbContext CreateHmsContext (string csName)
+    {
+        var cs = _config.GetConnectionString(csName)
+                 ?? throw new InvalidOperationException($"Connection string '{csName}' not found.");
+        return new HmsDbContext(new DbContextOptionsBuilder<HmsDbContext>().UseSqlServer(cs).Options);
+    }
+
+    public LisDbContext CreateLisContext(string csName)
+    {
+        var cs = _config.GetConnectionString(csName)
+                 ?? throw new InvalidOperationException($"Connection string '{csName}' not found.");
+        return new LisDbContext(new DbContextOptionsBuilder<LisDbContext>().UseSqlServer(cs).Options);
+    }
+
+    public ViphaDbContext CreateViphaContext(string csName)
+    {
+        var cs = _config.GetConnectionString(csName)
+                 ?? throw new InvalidOperationException($"Connection string '{csName}' not found.");
+        return new ViphaDbContext(new DbContextOptionsBuilder<ViphaDbContext>().UseSqlServer(cs).Options);
+    }
+}
