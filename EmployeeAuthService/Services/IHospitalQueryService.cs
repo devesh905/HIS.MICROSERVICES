d4ac9267fe6e2c78c0d933c33922862235f85738
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-07-25 10:54:26 +0530

    Add HospitalQueryService and HospitalRegistry for hospital data management

---
+++ EmployeeAuthService/Services/IHospitalQueryService.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeAuthService.Data;
+
+namespace EmployeeAuthService.Services;
+
+public interface IHospitalQueryService
+{
+    ViphaDbContext CreateHisContext(string connectionStringName);
+
+    Task<List<HospitalResult<T>>> QueryAllAsync<T>(
+        Func<ViphaDbContext, Task<T>> query);
+
+    Task<List<HospitalResult<T>>> QueryAllAsync<T>(
+        Func<ViphaDbContext, HospitalRegistry.HospitalEntry, Task<T>> query);
+}
+
+public record HospitalResult<T>(string Key, string Name, T Result);
