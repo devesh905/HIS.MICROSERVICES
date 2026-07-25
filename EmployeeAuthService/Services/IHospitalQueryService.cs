b7a2e5a0a53cbfe6ffdbc31841257346ecd6b408
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-07-25 11:14:44 +0530

    Add Entity Framework Core tools and Newtonsoft.Json packages; update solution file to include EmployeeAuthService project

--- EmployeeAuthService/Services/IHospitalQueryService.cs
+++ EmployeeAuthService/Services/IHospitalQueryService.cs
@@ -11,6 +11,11 @@ public interface IHospitalQueryService
 
     Task<List<HospitalResult<T>>> QueryAllAsync<T>(
         Func<ViphaDbContext, HospitalRegistry.HospitalEntry, Task<T>> query);
+
+    ViphaDbContext CreateViphaContext(string csName);
+
+    Task<List<HospitalResult<T>>> QueryAllViphaAsync<T>(
+  Func<ViphaDbContext, HospitalRegistry.HospitalEntry, Task<T>> query);
 }
 
 public record HospitalResult<T>(string Key, string Name, T Result);
