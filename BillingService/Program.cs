b67e89d4c85416ca522cc3549610cc1c433ace7b
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-07-25 08:25:43 +0530

    Add LabReportService with Swagger integration and logging configuration

--- BillingService/Program.cs
+++ BillingService/Program.cs
@@ -1,5 +1,6 @@
 using BillingService.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Shared.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,41 @@ builder.Services.AddSharedJwtAuthentication(builder.Configuration);
 
 builder.Services.AddCors(opt =>
     opt.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+// Authrization button in Swagger ke liye
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "HIS API",
+        Version = "v1"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter JWT Token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
